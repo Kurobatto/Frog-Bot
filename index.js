73025ae192be258a7f3d8c38e0e1f9d633bf5fd1
Kurobatto <matthewmcgrath.MTM@gmail.com>
2017-09-03 18:53:11 -0400

    Testing rolling dice command

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on('message', message => {
       });
     } else
 
-    if (message.content.toLowerCase().match(prefix + (/^r\s\dd\d\d\d\d/))) {
+    if (message.content.toLowerCase().match(prefix + (/^r\s\dd\d+/))) {
       message.channel.send("It worked!");
       //var messageArray = message.content.split(/[\sd]+/)
     }
