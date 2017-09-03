3feaff9932cdbaec0717ed15078eb485c91ff083
Kurobatto <matthewmcgrath.MTM@gmail.com>
2017-09-03 18:34:55 -0400

    Testing new method of message identification

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on('message', message => {
       });
     } else
 
-    if (message.content.toLowerCase().startsWith(prefix + 'r ' + (/[\d]+/) + 'd' + (/[\d]+/)) {
+    if (message.content.toLowerCase().startsWith(prefix + 'r *d*') {
       message.channel.send("It worked!");
       //var messageArray = message.content.split(/[\sd]+/)
     }
