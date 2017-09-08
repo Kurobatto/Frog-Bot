2b9ad7a32dd80fbf397f365c30cd063005aa3da6
MatthewMcGrath <MatthewMcGrath.MTM@gmail.com>
2017-09-08 13:57:00 -0400

    Fixing B emoji command

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on('message', message => {
     } else
 
     //B emoji version of ping command
-    if (message.content.toLowerCase() === (prefix + `:b:ing`)) {
+    if (message.content.toLowerCase() === (prefix + "🅱ing")) {
       //Creates a variable to save the user's message time
       var startTime = message.createdTimestamp;
 
