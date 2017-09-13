ab4442221025f6210bd8f7a73493f99a790f4f01
MatthewMcGrath <MatthewMcGrath.MTM@gmail.com>
2017-09-13 01:13:12 -0400

    Fixed node-schedule dependency

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ setInterval(() => {
   http.get('http://frogbotdiscord.herokuapp.com');
 }, 900000);
 
-var frogTime = schedule.scheduleJob({hour: 1, minute: 0, dayOfWeek: 3}, function(){
+var frogTime = schedule.scheduleJob({hour: 1, minute: 15, dayOfWeek: 3}, function(){
   client.channels.get('140946564901240832').send("", {
     file: "https://i.imgur.com/SPDD3R2.jpg"
   });
